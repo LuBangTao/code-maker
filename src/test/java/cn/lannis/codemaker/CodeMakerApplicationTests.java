2abaf399e49437a254b537e0b725c763084d0c06
LuBangTao <867401165@qq.com>
2020-11-30 10:48:30 +0800

    Initial commit

---
+++ src/test/java/cn/lannis/codemaker/CodeMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lannis.codemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
